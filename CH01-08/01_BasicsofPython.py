6c97bf4b26bd12ee436191544d7da792cb21cae7
maureen272 <maureen72@naver.com>
2024-09-05 16:06:17 +0900

    first class

---
+++ CH01-08/01_BasicsofPython.py
@@ -0,0 +1,18 @@
+#string연습 
+#터미널에서 파이썬 여는 방법 : python
+
+x = '''this if the first line
+this is the second line
+What's your name?'''
+print(x)
+
+a = 'good' + 'bye'
+print(a)
+b = ('a'+'b')*3
+print(b)
+c = 'a'*3 + 'b'*3
+print(c)
+d = 'ha'*3
+print(d)
+e = ("cha-"*2) + 'cha'
+print(e)
